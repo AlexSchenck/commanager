1ecf27156151aa5154b12983daca88e5e44427cb
Alex Schenck <AlexNSchenck@gmail.com>
2019-08-25 20:25:21 -0700

    Add a plus button on the action bar that opens the deck detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
 
-import { DecksComponent } from "./deck/decks.component"
-import { DeckDetailComponent } from "./deck/deck-detail.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-import { PlayComponent } from "./play/play.component";
+import { DecksComponent } from './deck/decks.component'
+import { DeckDetailComponent } from './deck/deck-detail.component';
+import { ItemsComponent } from './item/items.component';
+import { ItemDetailComponent } from './item/item-detail.component';
+import { PlayComponent } from './play/play.component';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/decks", pathMatch: "full" },
-    { path: "decks", component: DecksComponent },
-    { path: "deck/:id", component: DeckDetailComponent },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent },
-    { path: "play/:id", component: PlayComponent },
+    { path: '', redirectTo: '/decks', pathMatch: 'full' },
+    { path: 'deck', component: DeckDetailComponent },
+    { path: 'deck/:id', component: DeckDetailComponent },
+    { path: 'decks', component: DecksComponent },
+    { path: 'item/:id', component: ItemDetailComponent },
+    { path: 'items', component: ItemsComponent },
+    { path: 'play/:id', component: PlayComponent },
 
 ];
 
