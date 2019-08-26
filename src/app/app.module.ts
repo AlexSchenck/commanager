b8e357d35d5533654d08eb28ea94db7f7f0071c8
Alex Schenck <AlexNSchenck@gmail.com>
2019-08-25 22:26:44 -0700

    Import checkbox plugin, change color identity placeholders in deck detail component to checkboxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular';
+import { TNSCheckBoxModule } from '@nstudio/nativescript-checkbox/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { PlayComponent } from "./play/play.component"
     imports: [
         AppRoutingModule,
         NativeScriptModule,
-        NativeScriptUISideDrawerModule
+        NativeScriptUISideDrawerModule,
+        TNSCheckBoxModule
     ],
     declarations: [
         AppComponent,
