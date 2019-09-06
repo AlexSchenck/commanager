ca7c3661fa04a34556a9bd0cc5a98217a62c6661
Alex Schenck <AlexNSchenck@gmail.com>
2019-09-05 21:26:23 -0700

    Change deck detail to use data from database

--- src/app/deck/deck-detail.component.ts
+++ src/app/deck/deck-detail.component.ts
@@ -1,40 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ObservableArray } from 'tns-core-modules/data/observable-array/observable-array';
 
+import { ICardDefinition } from '../card/card-definition.interface';
 import { DataService } from '../data/data.service';
-import { IDeck } from './deck.interface';
 import { Color } from './color.enum';
+import { IDeck } from './deck.interface';
 
 @Component({
     selector: 'ns-deck-details',
     moduleId: module.id,
+    styleUrls: ['./deck-detail.component.css'],
     templateUrl: './deck-detail.component.html'
 })
-export class DeckDetailComponent {
+export class DeckDetailComponent implements OnDestroy {
+    public cards: ObservableArray<ICardDefinition>;
     public deck: IDeck;
     public title: string;
 
     public get color() { return Color; }
 
+    private _subscriptions: Subscription[]
+
     constructor(
         private _dataService: DataService,
         private _route: ActivatedRoute
     ) {
-        const id = +this._route.snapshot.params.id;
+        this.title = "New Deck";
+        this._subscriptions = [];
 
-        if (id) {
-            this._dataService.getDeck(id).subscribe(deck => {
-                this.deck = {
-                    id: 1,
-                    name: 'Izzet',
-                    commander: 'blah',
-                    colorIdentity: Color.Blue | Color.Red
-                }
-                this.title = this.deck.name;
-            });
-        } else {
-            this.title = 'New Deck';
-        }
+        this._subscriptions.push(this._dataService.getCardDefinitions().subscribe(cardDefinitions => {
+            this.cards = new ObservableArray(cardDefinitions);
+        }));
+
+        const id = +this._route.snapshot.params.id;
+        if (!id) return;
+
+        this._subscriptions.push(this._dataService.getDeck(id).subscribe(deck => {
+            this.deck = deck;
+            this.title = this.deck.name;
+        }));
+    }
+
+    public ngOnDestroy(): void {
+        this._subscriptions.forEach(subscription => subscription.unsubscribe());
     }
 
     public hasColor(color: Color): boolean {
